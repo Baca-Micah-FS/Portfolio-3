8c71e7e2a7aad0f6dcac65998f7701fa255264c2
Micah <MJBaca@student.fullsail.edu>
2026-02-04 11:04:33 -0600

    Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { MdVideoLibrary } from "react-icons/md";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -7,19 +8,23 @@ const Footer = () => {
       <div className="leftFooter">
         <form>
           <h3>Stay in the Loop</h3>
-          <p>Sign up for our newsletter and see whats new</p>
+          <p>Sign up for our newsletter to see whats new</p>
           <input type="email" placeholder="Enter your email" />
           <button type="button">Sign up</button>
         </form>
       </div>
 
       <div className="rightFooter">
-        <MdVideoLibrary />
-        <h3>Media Flux</h3>
+        <div className="footerLogoRow">
+          <MdVideoLibrary size={20} style={{ marginRight: "10px" }} />
+          <h3>Media-Flux</h3>
+        </div>
         <p>Your Personal hub for all things media</p>
+
         <div className="socialRow">
-          <FaInstagram />
-          <FaFacebook />
+          <FaInstagram size={30} />
+          <FaFacebook size={30} />
+          <FaXTwitter size={30} />
         </div>
       </div>
     </footer>
